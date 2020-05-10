cdbe71589c057bde8c00d816c68dec7d801d031b
khayriddine <khayribattikh@gmail.com>
2020-05-10 03:25:00 +0200

    setting up production enviornment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-   apiUrl :  'http://localhost:5000/',
-   hubUrl :  'http://localhost:5000/'
+   apiUrl :  'https://secret-hitler-backend.herokuapp.com/',
+   hubUrl :  'https://secret-hitler-backend.herokuapp.com/'
 };
 
 /*
